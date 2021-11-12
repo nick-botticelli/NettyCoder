190e6d0cb01d6a79104c63b55734e802e4d8bf33
Nick Botticelli <nick.s.botticelli@gmail.com>
2021-11-12 00:56:55 -0700

    Improve and add more config tests

--- src/test/java/website/nickb/nettycoder/config/NCConfigTests.java
+++ src/test/java/website/nickb/nettycoder/config/NCConfigTests.java
@@ -1,30 +1,60 @@
 package website.nickb.nettycoder.config;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
 import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class NCConfigTests
 {
+    private NCConfigManager configManager, customConfigManager;
+
     /**
-     * Passes if the required default configuration file resource exists
+     * Passes if the required default configuration file resource exists and it can be written to disk
      */
     @Test
+    @Order(1)
     public void Config_Loading(@TempDir Path tempPath) throws IOException
     {
-        //Path defaultTestConfigFilePath = tempPath.resolve(NCConfigManager.DEFAULT_CONFIG_PATH);
         Path customTestConfigFilePath = tempPath.resolve("config-test-custom.yml");
 
-        NCConfigManager configManager = new NCConfigManager();
+        configManager = new NCConfigManager();
         assertNotNull(configManager.getConfig());
         assertNotNull(configManager.getConfigFile());
 
-        configManager = new NCConfigManager(customTestConfigFilePath.toAbsolutePath().toString());
+        customConfigManager = new NCConfigManager(customTestConfigFilePath.toAbsolutePath().toString());
         assertNotNull(configManager.getConfig());
         assertNotNull(configManager.getConfigFile());
+    }
+
+    /**
+     * Passes if parsed content from configurations are correct
+     */
+    @Test
+    public void Config_Content()
+    {
+        NCConfig config = configManager.getConfig();
+        NCConfig config2 = customConfigManager.getConfig();
+
+        assertNotNull(config);
+        assertEquals(config.version, 0);
+        assertEquals(config.port, 18600);
+
+        assertNotNull(config2);
+        assertEquals(config2.version, 0);
+        assertEquals(config2.port, 18600);
+    }
+
+    /**
+     * Cleanup after tests (e.g., delete leftover files)
+     */
+    @AfterAll
+    public void cleanUp()
+    {
+        assertTrue(configManager.getConfigFile().delete());
     }
 }
