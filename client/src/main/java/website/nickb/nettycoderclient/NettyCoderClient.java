b0a0dac0e87cbdcc9c644a61bec4287378101bca
Nick Botticelli <nick.s.botticelli@gmail.com>
2021-11-20 15:33:13 -0700

    Add initial client subproject

---
+++ client/src/main/java/website/nickb/nettycoderclient/NettyCoderClient.java
@@ -0,0 +1,10 @@
+package website.nickb.nettycoderclient;
+
+import org.jetbrains.annotations.NotNull;
+
+public final class NettyCoderClient
+{
+    public static void main(@NotNull String[] args)
+    {
+    }
+}
